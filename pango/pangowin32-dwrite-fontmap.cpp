740c87ab41ebb47c470b87e5acce3adead8bd97f
Tulio Beloqui <tulio@pexip.com>
2024-07-11 13:53:33 +0200

    pangowin32: do not explode with a warning if we fail to fetch a font
    Just keep looping, the font family could have more fonts to be fetched.

--- pango/pangowin32-dwrite-fontmap.cpp
+++ pango/pangowin32-dwrite-fontmap.cpp
@@ -249,10 +249,7 @@ pango_win32_dwrite_font_map_populate_with_collection (PangoWin32FontMap     *map
 
           hr = family->GetFont (j, &font);
           if (FAILED (hr) || font == NULL)
-            {
-              g_warning ("IDWriteFontFamily::GetFont failed with error code %x\n", (unsigned)hr);
-              break;
-            }
+            continue;
 
           face = _pango_win32_get_dwrite_font_face_from_dwrite_font (font);
           if (face == NULL)
